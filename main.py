e3cfe2d703c167470f607952df14722f0f3b137b
Juan Pires <juan02pires@gmail.com>
2026-01-18 19:01:21 -0300

    feat: cria arquivo de saida para unificacao

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ arquivos_entrada = './input'
 
 # nome do arquivo de saída
 arquivo_saida = 'arquivo_unificado.RET'
+
+# abre ou cria, caso não exista, o arquivo de saída para escrita
+with open(arquivo_saida, 'w', encoding='utf-8') as saida:
+    pass
