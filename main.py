f106872a829a83346f5b0b1a7513592dcb9446f7
Juan Pires <juan02pires@gmail.com>
2026-02-01 23:51:48 -0300

    feat: cria diretorios de saida e backup

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import os
+from datetime import datetime
+import shutil
 
 ## também funciona para arquivos TXT
 ## apenas trocar as menções da extensão .ret/.RET para .txt
 
 # caminho da pasta onde estão os arquivos de entrada
-arquivos_entrada = './input'
+pasta_entrada = './input'
+
+# caminho das pastas para os arquivos de saída
+pasta_output = f'./output/{datetime.now().strftime('%d_%m_%Y')}'
+pasta_backup = f'./backup/{datetime.now().strftime('%d_%m_%Y')}'
+
+# criando as pastas de saída caso elas não existam
+for pasta in [pasta_output, pasta_backup]:
+    if not os.path.exists(pasta):
+        os.makedirs(pasta)
 
 # nome do arquivo de saída
-arquivo_saida = 'arquivo_unificado.RET'
+arquivo_saida = f'{pasta_output}/arquivo_unificado.RET'
 
 contador = 0
 
 # abre ou cria, caso não exista, o arquivo de saída para escrita
 with open(arquivo_saida, 'w', encoding='utf-8') as saida:
     # para cada arquivo na pasta, percorrendo em ordem alfabética
-    for nome_arquivo in sorted(os.listdir(arquivos_entrada)):
+    for nome_arquivo in sorted(os.listdir(pasta_entrada)):
         # se o arquivo contiver a extensão .ret
         if nome_arquivo.lower().endswith('.ret'):
             # salva o caminho do arquivo de entrada
-            caminho_arquivo = os.path.join(arquivos_entrada, nome_arquivo)
+            caminho_arquivo = os.path.join(pasta_entrada, nome_arquivo)
 
             try:
                 # abre o arquivo de entrada
